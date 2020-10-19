a2e55ffe4d22e7eb6fe3fe6391698fc17b692d20
Nathan E <10598429@my.uvu.edu>
2020-10-19 17:44:11 -0600

    Made a quick whitebox prototype for movement and camera controls

---
+++ Unity Projects Folder/Babies In Utero - Whitebox Prototype/Assets/camControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camControl : MonoBehaviour
+{
+	private Vector2 mD;
+	private Transform myBody;
+	
+    void Start()
+    {
+        myBody = this.transform.parent.transform;
+    }
+
+    void Update()
+    {
+        Vector2 mC = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+		mD += mC;
+		
+		this.transform.localRotation = Quaternion.AngleAxis (-mD.y, Vector3.right);
+		
+		myBody.localRotation = Quaternion.AngleAxis (mD.x, Vector3.up);
+		
+    }
+}
